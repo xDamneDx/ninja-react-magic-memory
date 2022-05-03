ac570b8835f073e972064d27bcb24e82d2863753
xDamneDx <xdamnedx@gmail.com>
2022-05-03 18:55:57 +0300

    Added matched property to cards

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import { SingleCard } from "./components/SingleCard";
 import "./App.css";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "/img/helmet-1.png", matched: false },
+  { src: "/img/potion-1.png", matched: false },
+  { src: "/img/ring-1.png", matched: false },
+  { src: "/img/scroll-1.png", matched: false },
+  { src: "/img/shield-1.png", matched: false },
+  { src: "/img/sword-1.png", matched: false },
 ];
 
 function App() {
@@ -36,10 +36,13 @@ function App() {
   useEffect(() => {
     if (choiceOne && choiceTwo) {
       if (choiceOne.src === choiceTwo.src) {
-        console.log("match");
+        setCards((prevCards) =>
+          prevCards.map((card) =>
+            card.src === choiceOne.src ? { ...card, matched: true } : card
+          )
+        );
         resetTurn();
       } else {
-        console.log("don't match");
         resetTurn();
       }
     }
